941cb4862882a84aecb1014743dacf6eeb5e1ae1
Chema <mrjark7@gmail.com>
2024-08-20 13:55:24 +0200

    changed colors and text

--- src/app/mrjark/page.tsx
+++ src/app/mrjark/page.tsx
@@ -2,29 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Mrjark() {
-  return(
-    <main className ="flex flex-col min-h-screen w-screen items-center justify-between p-24">
-      <div className="flex flex-col justify-center items-center w-2/3 h-80 bg-slate-400 rounded-3xl shadow-lg shadow-cyan-200">
+  return (
+    <main className="flex flex-col min-h-screen w-screen items-center justify-between p-24">
+      <div className="flex flex-col justify-center items-center w-2/3 h-80 bg-yellow-500 rounded-3xl shadow-lg shadow-yellow-400">
         <Image
-          className='flex mb-11'
-          alt='mrjark logo'
-          src='/mrjark.svg'
-          about='logo mrjark'
-          width={ 50 }
-          height={ 50 }
+          className="flex mb-11"
+          alt="mrjark logo"
+          src="/mrjark.svg"
+          about="logo mrjark"
+          width={50}
+          height={50}
         />
         <h1 className=" font-bold mb-8 text-4xl">W.I.P.</h1>
         <Link
-          className="rounded-full text-blue-300 italic bg-blue-900 cursor-pointer px-6 py-1 transition-all duration-300 hover:scale-105 hover:shadow-sm hover:shadow-cyan-200"
-          href='/'
+          className="rounded-full text-yellow-300 italic bg-yellow-900 cursor-pointer px-6 py-1 transition-all duration-300 hover:scale-105 hover:shadow-sm hover:shadow-yellow-200"
+          href="/"
         >
           Go Back
         </Link>
-        <div className=' lg:hidden md:hidden sm:visible'>
-          <p className='text-center text-lg mt-5 text-slate-100'>No, esto tampoco es responsive</p>
-          <span className='flex text-center flex-col'>🙄</span>
+        <div className=" lg:hidden md:hidden sm:visible">
+          <p className="text-center text-lg mt-5 text-slate-100">
+            No, esto tampoco es responsive
+          </p>
+          <span className="flex text-center flex-col">🙄</span>
         </div>
       </div>
     </main>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,82 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import {AiFillGithub, AiFillInstagram, AiFillLinkedin} from 'react-icons/ai'
+import Image from "next/image";
+import Link from "next/link";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-24">
-      <div className='bg-gray-950 py-12 px-20 rounded-3xl  shadow-slate-700 shadow-xl'>
-        <div className='flex flex-col justify-center items-center gap-4'>
-          <h1 
-            className='text-5xl text-teal-50 opacity-80 font-bold'
-          >Hola soy Chema aka MrJark por las redes 🙋🏽‍♂️</h1>
-          <div className=' flex flex-row items-center justify-center gap-6'>
-            <Image 
-              className='flex rounded-full'
-              alt='mrjark - chema ferrandez photo profile'
-              src='https://unavatar.io/github/mrjark'
-              about='mrjark photo'
+      <div className="bg-gray-950 py-12 px-20 rounded-3xl  shadow-slate-700 shadow-xl">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <h1 className="text-5xl text-teal-50 opacity-80 font-bold">
+            Hola soy Chema aka mrjark por las redes 🙋🏽‍♂️
+          </h1>
+          <article className=" flex flex-row items-center justify-center gap-6">
+            <Image
+              className="flex rounded-full"
+              alt="mrjark - chema ferrandez photo profile"
+              src="https://unavatar.io/github/mrjark"
+              about="mrjark photo"
               width={200}
               height={200}
             />
-            <h2 className='text-2xl text-white'>
-              Fanático las Startup 🦄<br />Exploro, aprendo y comparto píldoras sobre <strong className='text-teal-400 italic'>Growth</strong>, <strong className='text-blue-400 italic'>Behavioural</strong> & <strong className='text-yellow-400 italic'>Startups</strong> en mis redes y blog.
-            </h2>
-
-          </div>
+            <section className="flex flex-col gap-5">
+              <h2 className="text-3xl text-white">
+                Fanático de Growth y las Startup 🦄
+              </h2>
+              <h3 className="text-xl">
+                Esta página es una preview a mi blog el cual está en
+                construcción aunque no te voy a engañar, hay ciertas partes de
+                la ideación que aún estoy trabajando en ella 😅
+              </h3>
+              <p className="text-md text-zinc-400">
+                Si quieres contactarme, te dejo mi LinkedIn por aquí abajo, que
+                es donde estoy más activo.
+              </p>
+            </section>
+          </article>
         </div>
-        <div className='flex justify-center pt-10 gap-8'>
-          <Link 
-            href='https://linkedin.com/in/mrjark'
-            className='flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline'
+        <div className="flex justify-center pt-10 gap-8">
+          <Link
+            href="https://linkedin.com/in/mrjark"
+            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
-            <AiFillLinkedin className = 'text-white text-2xl'/>
+            <AiFillLinkedin className="text-white text-2xl" />
             LinkedIn
           </Link>
-          <Link 
-            href='https://www.instagram.com/mrjark_/'
-            className='flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline'
+          <Link
+            href="https://www.instagram.com/mrjark_/"
+            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
-            <AiFillInstagram className = 'text-white text-2xl'/>
+            <AiFillInstagram className="text-white text-2xl" />
             Instagram
           </Link>
-          <Link 
-            href='https://github.com/MrJark'
-            className='flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline'
+          <Link
+            href="https://github.com/MrJark"
+            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
-            <AiFillGithub className = 'text-white text-2xl'/>
+            <AiFillGithub className="text-white text-2xl" />
             GitHub
           </Link>
-          <Link 
-            href='./mrjark'
-            className='flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 opacity-50 hover:underline'
+          <Link
+            href="./mrjark"
+            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 opacity-50 hover:underline"
           >
-            <Image src='mrjark.svg' alt='logo de mrjark' width={24} height={24} />
+            <Image
+              src="mrjark.svg"
+              alt="logo de mrjark"
+              width={24}
+              height={24}
+            />
             mrjark.com (in progres)
           </Link>
         </div>
-        <div className=' lg:hidden md:hidden sm:visible'>
-          <p className='text-center text-lg mt-5 text-slate-400'>Lo sé, no es responsive</p>
-          <span className='flex text-center flex-col'>🙄</span>
+        <div className=" lg:hidden md:hidden sm:visible">
+          <p className="text-center text-lg mt-5 text-slate-400">
+            Lo sé, no es responsive
+          </p>
+          <span className="flex text-center flex-col">🙄</span>
         </div>
-          
       </div>
     </main>
-  )
+  );
 }
