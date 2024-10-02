80453352eb0bf5a6801ef0e8ea6a3392b9633823
Chema <mrjark7@gmail.com>
2024-10-02 09:48:07 +0200

    deleted packages and reinstalled, changes links and favicons and added Substack link

--- src/app/mrjark/page.tsx
+++ src/app/mrjark/page.tsx
@@ -4,25 +4,25 @@ import Link from "next/link";
 export default function Mrjark() {
   return (
     <main className="flex flex-col min-h-screen w-screen items-center justify-between p-24">
-      <div className="flex flex-col justify-center items-center w-2/3 h-80 bg-yellow-500 rounded-3xl shadow-lg shadow-yellow-400">
+      <div className="flex flex-col justify-center items-center w-2/3 h-80 bg-zinc-500 rounded-3xl shadow-lg shadow-zinc-400">
         <Image
           className="flex mb-11"
           alt="mrjark logo"
           src="/mrjark.svg"
-          about="logo mrjark"
+          about="mrjark logo"
           width={50}
           height={50}
         />
         <h1 className=" font-bold mb-8 text-4xl">W.I.P.</h1>
         <Link
-          className="rounded-full text-yellow-300 italic bg-yellow-900 cursor-pointer px-6 py-1 transition-all duration-300 hover:scale-105 hover:shadow-sm hover:shadow-yellow-200"
+          className="rounded-full text-zinc-300 italic bg-zinc-900 cursor-pointer px-6 py-1 transition-all duration-300 hover:scale-105 hover:shadow-sm hover:shadow-zinc-200"
           href="/"
         >
           Go Back
         </Link>
         <div className=" lg:hidden md:hidden sm:visible">
           <p className="text-center text-lg mt-5 text-slate-100">
-            No, esto tampoco es responsive
+            No, tampoco es responsive
           </p>
           <span className="flex text-center flex-col">🙄</span>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { SiSubstack } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -8,20 +9,20 @@ export default function Home() {
       <div className="bg-gray-950 py-12 px-20 rounded-3xl  shadow-slate-700 shadow-xl">
         <div className="flex flex-col justify-center items-center gap-4">
           <h1 className="text-5xl text-teal-50 opacity-80 font-bold">
-            Hola soy Chema aka mrjark por las redes 🙋🏽‍♂️
+            Yep aquí mrjark, pero en offline me dicen Chema
           </h1>
           <article className=" flex flex-row items-center justify-center gap-6">
             <Image
               className="flex rounded-full"
               alt="mrjark - chema ferrandez photo profile"
-              src="https://unavatar.io/github/mrjark"
+              src="https://unavatar.io/github/immrjark"
               about="mrjark photo"
               width={200}
               height={200}
             />
             <section className="flex flex-col gap-5">
               <h2 className="text-3xl text-white">
-                Fanático de Growth y las Startup 🦄
+                Fanático de Growth, las Startup y el Tattoo
               </h2>
               <h3 className="text-xl">
                 Esta página es una preview a mi blog el cual está en
@@ -29,37 +30,46 @@ export default function Home() {
                 la ideación que aún estoy trabajando en ella 😅
               </h3>
               <p className="text-md text-zinc-400">
-                Si quieres contactarme, te dejo mi LinkedIn por aquí abajo, que
-                es donde estoy más activo.
+                Si quieres contactarme, te dejo mis redes por aquí. Y si quieres
+                saber más sobre cómo voy haciendo las cosas, en Substack me
+                puedes encontrar escribiendo sobre mi día a día y la preview de
+                este blog
               </p>
             </section>
           </article>
         </div>
         <div className="flex justify-center pt-10 gap-8">
           <Link
-            href="https://linkedin.com/in/immrjark"
-            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
+            href="https://substack.com/@mrjark"
+            className="flex justify-center content-center items-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
+          >
+            <SiSubstack className="text-white text-xl" />
+            Substack
+          </Link>
+          <Link
+            href="https://linkedin.com/in/chemaferrandez"
+            className="flex justify-center content-center items-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
             <AiFillLinkedin className="text-white text-2xl" />
             LinkedIn
           </Link>
           <Link
             href="https://www.instagram.com/immrjark/"
-            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
+            className="flex justify-center content-center items-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
             <AiFillInstagram className="text-white text-2xl" />
             Instagram
           </Link>
           <Link
             href="https://github.com/immrJark"
-            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
+            className="flex justify-center content-center items-center gap-2 text-lg italic font-medium text-purple-200 hover:underline"
           >
             <AiFillGithub className="text-white text-2xl" />
             GitHub
           </Link>
           <Link
             href="./mrjark"
-            className="flex justify-center content-center gap-2 text-lg italic font-medium text-purple-200 opacity-50 hover:underline"
+            className="flex justify-center content-center items-center gap-2 text-lg italic font-medium text-purple-200 opacity-50 hover:underline"
           >
             <Image
               src="mrjark.svg"
@@ -67,7 +77,7 @@ export default function Home() {
               width={24}
               height={24}
             />
-            mrjark.com (in progres)
+            mrjark.com (in progress)
           </Link>
         </div>
         <div className=" lg:hidden md:hidden sm:visible">
